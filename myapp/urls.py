f018de92c5c167cccb224c7af69a936ce4260040
Diane Corriette <dcorriette@gmail.com>
2025-01-16 15:38:14 +0000

    create contact app

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
-from django.views.generic.base import TemplateView
+from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+    path("ckeditor/", include("ckeditor_uploader.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
