35537fac7315c7a22ba7270cf06811adf1110afc
djangify <djangify@gmail.com>
2025-09-22 17:06:44 +0100

    Update sitemap

--- myapp/sitemaps.py
+++ myapp/sitemaps.py
@@ -4,74 +4,90 @@ from portfolio.models import Portfolio, Technology
 from news.models import Post, Category
 from django.utils import timezone
 
+
 class StaticViewSitemap(Sitemap):
     priority = 0.5
-    changefreq = 'weekly'
+    changefreq = "weekly"
 
     def items(self):
-        return ['core:home', 'core:privacy_policy', 'core:cookie_policy', 'core:terms_conditions', 
-                'portfolio:portfolio_list', 'news:list']
+        return [
+            "core:home",
+            "core:privacy_policy",
+            "core:cookie_policy",
+            "core:terms_conditions",
+            "portfolio:portfolio_list",
+            "news:list",
+            "core:about",
+            "core:tech_va",
+        ]
 
     def location(self, item):
         url = reverse(item)
         # Remove any protocol prefix if it exists to prevent duplication
-        if url.startswith('http'):
-            url_parts = url.split('://', 1)
+        if url.startswith("http"):
+            url_parts = url.split("://", 1)
             if len(url_parts) > 1:
                 return url_parts[1]
         return url
 
+
 class PortfolioSitemap(Sitemap):
     priority = 1.0  # Highest priority as requested
-    changefreq = 'weekly'
+    changefreq = "weekly"
 
     def items(self):
-        return Portfolio.objects.filter(status='published')
+        return Portfolio.objects.filter(status="published")
 
     def lastmod(self, obj):
         return obj.updated_at
 
     def location(self, obj):
-        return reverse('portfolio:portfolio_detail', kwargs={'slug': obj.slug})
+        return reverse("portfolio:portfolio_detail", kwargs={"slug": obj.slug})
+
 
 class TechnologySitemap(Sitemap):
     priority = 0.8
-    changefreq = 'monthly'
+    changefreq = "monthly"
 
     def items(self):
         return Technology.objects.all()
 
     def location(self, obj):
-        return reverse('portfolio:portfolio_by_technology', kwargs={'technology_slug': obj.slug})
+        return reverse(
+            "portfolio:portfolio_by_technology", kwargs={"technology_slug": obj.slug}
+        )
+
 
 class NewsSitemap(Sitemap):
     priority = 0.7
-    changefreq = 'daily'
+    changefreq = "daily"
 
     def items(self):
-        return Post.objects.filter(status='published', publish_date__lte=timezone.now())
+        return Post.objects.filter(status="published", publish_date__lte=timezone.now())
 
     def lastmod(self, obj):
         return obj.updated
 
     def location(self, obj):
-        return reverse('news:detail', kwargs={'slug': obj.slug})
+        return reverse("news:detail", kwargs={"slug": obj.slug})
+
 
 class NewsCategorySitemap(Sitemap):
     priority = 0.6
-    changefreq = 'weekly'
+    changefreq = "weekly"
 
     def items(self):
         return Category.objects.all()
 
     def location(self, obj):
-        return reverse('news:category', kwargs={'slug': obj.slug})
+        return reverse("news:category", kwargs={"slug": obj.slug})
+
 
 # Combine all sitemaps in a dictionary
 sitemaps = {
-    'static': StaticViewSitemap,
-    'portfolio': PortfolioSitemap,
-    'technology': TechnologySitemap,
-    'news': NewsSitemap,
-    'news_category': NewsCategorySitemap,
+    "static": StaticViewSitemap,
+    "portfolio": PortfolioSitemap,
+    "technology": TechnologySitemap,
+    "news": NewsSitemap,
+    "news_category": NewsCategorySitemap,
 }
